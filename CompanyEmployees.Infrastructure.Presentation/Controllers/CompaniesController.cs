5c8454067c1faac5b37120810994c0b7235b1997
Alex <puyaalexxx@gmail.com>
2025-05-21 13:40:27 +0300

    custom model binder for company ids

--- CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Infrastructure.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Core.Services.Abstractions;
+using CompanyEmployees.Infrastructure.Presentation.ModelBinders;
 using Microsoft.AspNetCore.Mvc;
 using Shared.DataTransferObjects;
 
@@ -32,7 +33,7 @@ public class CompaniesController : ControllerBase
     }
 
     [HttpGet("collection/({ids})", Name = "CompanyCollection")]
-    public IActionResult GetCompanyCollection(IEnumerable<Guid> ids)
+    public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
     {
         var companies = _service.CompanyService.GetByIds(ids, trackChanges: false);
         
